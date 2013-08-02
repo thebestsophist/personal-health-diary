47669d7040d5b033ec4c51cbbd6bae8633fc2e3e
Bernard Yu <hello@ayellowraven.com>
2013-08-02 16:18:03 -0500

    major changes, all prepped for first ensemble test

--- install/includes/config_default.php
+++ install/includes/config_default.php
@@ -14,7 +14,7 @@
 			'pagination_maximum_rows' => '20',
 			'lang' => 'en',
 			'pages_table_nest_children' => 'no',
-			'version' => VERSION,
+			'version' => '2.3.2',
 			'cookie_prefix' => 'sym-',
 			'session_gc_divisor' => '10',
 		),
@@ -50,7 +50,7 @@
 
 		###### GENERAL ######
 		'general' => array(
-			'sitename' => 'Symphony CMS',
+			'sitename' => 'Health Diary - dev',
 		),
 		########
 
@@ -71,10 +71,10 @@
 
 		###### REGION ######
 		'region' => array(
-			'time_format' => 'g:i a',
-			'date_format' => 'm/d/Y',
+			'time_format' => 'H:i:s',
+			'date_format' => 'Y-m-d',
 			'datetime_separator' => ' ',
-			'timezone' => null
+			'timezone' => 'America/Chicago',
 		),
 		########
 
@@ -83,6 +83,69 @@
 		'image' => array(
 			'cache' => '1',
 			'quality' => '90',
+			'disable_regular_rules' => 'no',
+			'disable_upscaling' => 'no',
+		),
+		########
+
+
+		###### MAINTENANCE_MODE ######
+		'maintenance_mode' => array(
+			'enabled' => 'no',
+		),
+		########
+
+
+		###### GLOBALRESOURCELOADER ######
+		'globalresourceloader' => array(
+			'ds-names' => 'auth_token,current_user,datetime,useragent_details',
+		),
+		########
+
+
+		###### DOCUMENTATION ######
+		'documentation' => array(
+			'text-formatter' => 'markdown_with_purifier',
+			'button-text' => 'Documentation',
+		),
+		########
+
+
+		###### SORTING ######
+		'sorting' => array(
+			'section_body-parts_sortby' => '38',
+			'section_body-parts_order' => 'desc',
+			'data_sources_index_sortby' => 'name',
+			'data_sources_index_order' => 'asc',
+			'section_sleep-diary_sortby' => '17',
+			'section_sleep-diary_order' => 'asc',
+		),
+		########
+
+
+		###### USERAGENT_DETAILS ######
+		'useragent_details' => array(
+			'geoplugin' => 'yes',
+		),
+		########
+
+
+		###### MEMBERS ######
+		'members' => array(
+			'cookie-prefix' => 'sym-members',
+			'section' => '10',
+		),
+		########
+
+
+		###### APIPAGE ######
+		'apipage' => array(
+			'default-format' => 'json',
+			'param-selector' => 'url-format',
+			'jsonp-var' => 'api_read',
+			'jsonp-callback' => 'api_page',
+			'header-override' => 'no',
+			'disable-content-length' => 'yes',
 		),
 		########
 	);
---
+++ manifest/config.php
@@ -0,0 +1,180 @@
+<?php
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'pagination_maximum_rows' => '20',
+			'lang' => 'en',
+			'pages_table_nest_children' => 'no',
+			'version' => '2.3.2',
+			'cookie_prefix' => 'sym-',
+			'session_gc_divisor' => '10',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => 'health-diary',
+			'password' => 'ni9624',
+			'db' => 'dbd_health-diary',
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'no',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'sitename' => 'Health Diary - dev',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0644',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0755',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'H:i:s',
+			'date_format' => 'Y-m-d',
+			'datetime_separator' => ' ',
+			'timezone' => 'America/Chicago',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+			'disable_regular_rules' => 'no',
+			'disable_upscaling' => 'no',
+		),
+		########
+
+
+		###### MAINTENANCE_MODE ######
+		'maintenance_mode' => array(
+			'enabled' => 'no',
+		),
+		########
+
+
+		###### GLOBALRESOURCELOADER ######
+		'globalresourceloader' => array(
+			'ds-names' => 'auth_token,current_user,datetime,useragent_details',
+		),
+		########
+
+
+		###### DOCUMENTATION ######
+		'documentation' => array(
+			'text-formatter' => 'markdown_with_purifier',
+			'button-text' => 'Documentation',
+		),
+		########
+
+
+		###### SORTING ######
+		'sorting' => array(
+			'section_body-parts_sortby' => '38',
+			'section_body-parts_order' => 'desc',
+			'data_sources_index_sortby' => 'name',
+			'data_sources_index_order' => 'asc',
+			'section_sleep-diary_sortby' => '17',
+			'section_sleep-diary_order' => 'asc',
+		),
+		########
+
+
+		###### USERAGENT_DETAILS ######
+		'useragent_details' => array(
+			'geoplugin' => 'yes',
+		),
+		########
+
+
+		###### EMAIL ######
+		'email' => array(
+			'default_gateway' => 'sendmail',
+		),
+		########
+
+
+		###### EMAIL_SENDMAIL ######
+		'email_sendmail' => array(
+			'from_name' => null,
+			'from_address' => null,
+		),
+		########
+
+
+		###### EMAIL_SMTP ######
+		'email_smtp' => array(
+			'from_name' => null,
+			'from_address' => null,
+			'host' => '127.0.0.1',
+			'port' => '25',
+			'secure' => 'no',
+			'auth' => '0',
+			'username' => null,
+			'password' => null,
+		),
+		########
+
+
+		###### MEMBERS ######
+		'members' => array(
+			'cookie-prefix' => 'sym-members',
+			'section' => '10',
+		),
+		########
+
+
+		###### APIPAGE ######
+		'apipage' => array(
+			'default-format' => 'json',
+			'param-selector' => 'url-format',
+			'jsonp-var' => 'api_read',
+			'jsonp-callback' => 'api_page',
+			'header-override' => 'no',
+			'disable-content-length' => 'yes',
+		),
+		########
+	);
